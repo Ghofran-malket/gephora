9c87ba5124af2afd220d2dc6eafef9de0c80b0ed
Ghofran <gh.malket@gmail.com>
2024-07-11 16:51:07 +0200

    ajouter un formulaire d'inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/notre-selection", "Notre Selection", "/pages/notre-selection.html"),
-    new Route("/signin", "SignIn", "/pages/signin.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
