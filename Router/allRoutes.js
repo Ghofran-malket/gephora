f9abdfa3d42a8f4c77f1bd8c81dc4f87e49a3797
Ghofran <gh.malket@gmail.com>
2024-07-12 16:08:34 +0200

    vérifier les champs d'un formulaire d'inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/notre-selection", "Notre Selection", "/pages/notre-selection.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html","/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
 ];
 
