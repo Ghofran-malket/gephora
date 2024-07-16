8fe8bccfb1c5126f97d1b0c12177718c67140ab0
Ghofran <gh.malket@gmail.com>
2024-07-16 12:02:42 +0200

    Eviter faille Xss injection html dans la page "NotreSelection"

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
-    new Route("/notre-selection", "Notre Selection", "/pages/notre-selection.html", []),
+    new Route("/notre-selection", "Notre Selection", "/pages/notre-selection.html", [], "js/notre-selection.js"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
